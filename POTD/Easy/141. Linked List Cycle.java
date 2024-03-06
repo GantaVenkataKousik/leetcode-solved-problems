e99aa407498167839b102426d71a9b10eff62e4a
gantavenkatakousik <venkatakousikcse01@gmail.com>
2024-03-06 22:31:54 +0530

    Create 141. Linked List Cycle.java

---
+++ POTD/Easy/141. Linked List Cycle.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+                if(head==null || head.next==null)return false;
+        ListNode fast = head , slow = head;
+        do{
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)return true;
+        }while(fast!=null && fast.next!=null);
+        return false;
+    }
+}
