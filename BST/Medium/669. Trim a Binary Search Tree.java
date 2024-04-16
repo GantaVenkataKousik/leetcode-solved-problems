9bed8d9799b13a90e8721d43f733cd8a39341e46
gantavenkatakousik <venkatakousikcse01@gmail.com>
2024-04-16 23:46:36 +0530

    Create 669. Trim a Binary Search Tree.java

---
+++ BST/Medium/669. Trim a Binary Search Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root==null)return root;
+
+        root.left=trimBST(root.left , low,high);
+        root.right=trimBST(root.right,low,high);
+
+        if(root.val < low)return root.right;
+        if(root.val > high)return root.left;
+
+        return root;
+    }
+}
